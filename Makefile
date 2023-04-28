9b84dce5dfa9c9ea4c2ce8ee3670821cd96e2142
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-04-28 16:28:55 -0300

    chore(update): version control
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,40 @@
-build:
-	go build -v -a -ldflags "-X 'localhost/ngtools/cmd.version=v0.1.0'" -o bin/ngtools main.go
+ifeq ($(origin VERSION), undefined)
+VERSION := $(shell git describe --tags --always --match='v*')
+endif
 
+GIT_TREE_STATE:="dirty"
+ifeq (, $(shell git status --porcelain=2>/dev/null))
+  GIT_TREE_STATE="clean"
+endif
+GIT_COMMIT:=$(shell git rev-parse HEAD)
+
+VERSION_PACKAGE=localhost/ngtools/cmd
+
+SYS_VERSION:="v0.1.0"
+
+GO_LDFLAGS += \
+	"-extldflags=-static" \
+  -X $(VERSION_PACKAGE).Version=$(SYS_VERSION) \
+  -X $(VERSION_PACKAGE).GitVersion=$(VERSION) \
+  -X $(VERSION_PACKAGE).GitCommit=$(GIT_COMMIT) \
+  -X $(VERSION_PACKAGE).GitTreeState=$(GIT_TREE_STATE) \
+  -X $(VERSION_PACKAGE).BuildDate=$(shell date -u +'%Y-%m-%dT%H:%M:%SZ')
+
+.PHONY: all
+all: format build
+
+.PHONY: build
+build:
+	go build -v -a -ldflags "$(GO_LDFLAGS)" -o bin/ngtools main.go
+
+.PHONY: run
 run: build
 	./bin/ngtools start -protocol http -port 3031
+
+.PHONY: tidy
+tidy: 
+	@go mod tidy
+
+.PHONY: format
+format: 
+	@gofmt -s -w ./
--- cmd/version.go
+++ cmd/version.go
@@ -8,7 +8,18 @@ import (
 	"fmt"
 )
 
-var version = "0.0.0-dev"
+var (
+	// NGTools version
+	Version = "0.0.0-dev"
+	// GitVersion is the semantic version number.
+	GitVersion = "v0.0.0-master+$Format:%h$"
+	// BuildDate is the build time of ISO8601 format, the output of the $(date -u +'%Y-%m-%dT%H:%M:%SZ') command.
+	BuildDate = "1970-01-01T00:00:00Z"
+	// GitCommit is the SHA1 value of Git, the output of the $(git rev-parse HEAD) command.
+	GitCommit = "$Format:%H$"
+	// GitTreeState Represents the state of the Git repository at build time, with possible values: clean, dirty.
+	GitTreeState = ""
+)
 
 func (cli *Cli) version() {
 
@@ -17,6 +28,10 @@ func (cli *Cli) version() {
 	// stdout, _ := cmd.CombinedOutput()
 	// ngrokVersion := strings.Trim(strings.TrimPrefix(string(stdout), "git version "), " \r\n")
 
-	fmt.Println(version)
+	fmt.Println("NGTools ", Version)
+	fmt.Println(" Git Version    ", GitVersion)
+	fmt.Println(" Build Date     ", BuildDate)
+	fmt.Println(" Git Commmit    ", GitCommit)
+	fmt.Println(" Git Tree State ", GitCommit)
 
 }
